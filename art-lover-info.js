6a34f185b0953a6cf2bd7dc72988bbb2755e78aa
Folashade Lateef <flatee90@gmail.com>
2024-09-20 08:39:58 -0500

    adding about me page. having trouble with save function for javascript

---
+++ art-lover-info.js
@@ -0,0 +1,16 @@
+console.log("Hello from art lover info");
+
+let emailInput = document.getElementById("email-input");
+let fullNameInput = document.getElementById("full-name-input");
+let saveButton = document.getElementById("save-Button");
+
+console.log(emailInput);
+console.log(fullNameInput);
+
+
+function handleSaveData() {
+    console.log("hi");
+
+}
+
+saveButton.addEventListener("click", handleSaveData);
