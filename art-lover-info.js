9bb9dc4c1558d80179786eb1018d12a258da816c
Folashade Lateef <flatee90@gmail.com>
2024-09-20 12:29:49 -0500

    working on splashpage

--- art-lover-info.js
+++ art-lover-info.js
@@ -1,16 +1,27 @@
 console.log("Hello from art lover info");
 
 let emailInput = document.getElementById("email-input");
-let fullNameInput = document.getElementById("full-name-input");
-let saveButton = document.getElementById("save-Button");
+let fullNameInput = document.getElementById("full-Name-input");
+let savebutton = document.getElementById("save-button");
 
 console.log(emailInput);
 console.log(fullNameInput);
 
 
-function handleSaveData() {
-    console.log("hi");
+function handleSavejoinus() {
+    let fullName = fullNameInput.value;
+    let email = emailInput.value;
+
+    console.log(fullName, email);
+
+    localStorage.setItem("fullName", fullName);
+    localStorage.setItem("email", email);
+
+
 
 }
 
-saveButton.addEventListener("click", handleSaveData);
+savebutton.addEventListener("click", handleSavejoinus);
+
+let myEmail = localStorage.getitem("email");
+let myName = localStorage.getitem("fullName");
