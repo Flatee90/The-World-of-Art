0977ae29b32e38629a85f433e37fc04754b583e9
Folashade Lateef <flatee90@gmail.com>
2024-09-25 16:11:39 -0500

    added font aweson to about.html, finished fetch code

--- theworldofart.js
+++ theworldofart.js
@@ -1,12 +1,27 @@
-console.log("Hello from th world of art!");
+console.log("hello from The World of art");
 
 let fetchButton = document.getElementById("fetch-button");
 let Objectnumber = document.getElementById("object-number");
+let artOutput = document.getElementById("art-output");
 
 async function handlefetchobject() {
     console.log("Welcome Art Lover!");
     let Objectnumber = objectInput.value;
-    let response = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search`);
+    let url = `https://collectionapi.metmuseum.org/public/collection/v1/search/${Objectnumber}`;
+    console.log(url);
+    try {
+        let result = await fetch(url);
+        if (!result.ok) {
+            throw new Error("failed to fetch data");
+        }
+        let data = await result.json();
+        console.log(data);
+    } catch (error) {
+        artOutput.textContent = "Error fetchining data.";
+        console.log(error);
+
+    }
+
 }
 
 
