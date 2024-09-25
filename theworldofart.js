cfc128b1057459202c67f0475da5e2fa44e8751a
Folashade Lateef <flatee90@gmail.com>
2024-09-24 20:55:38 -0500

    added fetch code and about page

--- theworldofart.js
+++ theworldofart.js
@@ -1,1 +1,15 @@
-let painting
+console.log("Hello from th world of art!");
+
+let fetchButton = document.getElementById("fetch-button");
+let Objectnumber = document.getElementById("object-number");
+
+async function handlefetchobject() {
+    console.log("Welcome Art Lover!");
+    let Objectnumber = objectInput.value;
+    let response = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search`);
+}
+
+
+
+
+fetchButton.addEventListener("click", handlefetchobject);
